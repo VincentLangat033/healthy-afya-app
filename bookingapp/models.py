3516228b19079f55b602474148426e0390751714
VincentLangat033 <kimutaiketer033@gmail.com>
2022-11-16 00:39:56 -0500

    add the changes to the views.py

--- bookingapp/models.py
+++ bookingapp/models.py
@@ -3,13 +3,21 @@ from django.contrib.auth.models import User
 
 
 class Patient(models.Model):
+    M = 'Male'
+    F = 'Female'
+
+    GENDER_CHOICES = [
+        (M, 'Male'),
+        (F, 'Female'),
+    ]
     first_name = models.CharField(max_length = 255)
     last_name = models.CharField(max_length = 255)
     # slug = models.SlugField(default='-')
-    age = models.IntegerField()
+    age = models.IntegerField(blank=True)
     email = models.EmailField(unique=True)
     phone = models.CharField(max_length=255)
     birth_date = models.DateField(null=True, blank=True)
+    gender = models.CharField(max_length=10, choices=GENDER_CHOICES, default=F)
     created_at = models.DateTimeField(auto_now_add = True)
     last_update = models.DateTimeField(auto_now = True)
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='user_profile',null=True)
--- members/views.py
+++ members/views.py
@@ -2,11 +2,12 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import  messages
 from django.contrib.auth.forms import UserCreationForm
+from forms import RegisterUserForm
 
 
 def register_user(request):
     if request.method == "POST":
-        form = UserCreationForm(request.POST)
+        form = RegisterUserForm(request.POST)
         if form.is_valid():
             form.save()
             username = form.cleaned_data['username']
@@ -17,8 +18,23 @@ def register_user(request):
             return redirect('home')
 
     else:
-        form = UserCreationForm()
+        form = RegisterUserForm()
     return render(request,'authenticate/register_user.html', {'form': form} )
+# def register_user(request):
+#     if request.method == "POST":
+#         form = UserCreationForm(request.POST)
+#         if form.is_valid():
+#             form.save()
+#             username = form.cleaned_data['username']
+#             password = form.cleaned_data['password1']
+#             user = authenticate(username=username, password=password)
+#             login(request, user)
+#             messages.success(request, ("Registration succesful"))
+#             return redirect('home')
+
+#     else:
+#         form = UserCreationForm()
+#     return render(request,'authenticate/register_user.html', {'form': form} )
 
 
 def login_user(request):
