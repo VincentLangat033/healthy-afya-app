789d41c9ac0e9247318304faeb0df6f8175849d2
VincentLangat033 <kimutaiketer033@gmail.com>
2022-11-25 03:00:49 -0500

    add a redirects decorator

--- bookingapp/decorators.py
+++ bookingapp/decorators.py
@@ -23,3 +23,21 @@ def allowed_users(allowed_roles=[]):
                 return redirect('home')
         return wrapper_func
     return decorator
+
+
+def home_redirect(view_func):
+    def wrapper_func(request, *args ,**kwargs):
+        group = None
+        if request.user.groups.exists():
+            group = request.user.groups.all()[0].name
+        if group == 'patient':
+            return redirect('patient-dashboard')
+        if group == 'doctors':
+            return redirect('doctor-dashboard')
+        if group == 'admin':
+            return view_func(request, *args, **kwargs)
+        else:
+            return view_func(request, *args, **kwargs)
+
+    return wrapper_func
+   
