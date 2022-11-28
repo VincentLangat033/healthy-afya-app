952d7908431a5f0bd16acb88eb89f6a322edf70d
VincentLangat033 <kimutaiketer033@gmail.com>
2022-11-28 03:42:26 -0500

    configure apps.py to import signals

--- bookingapp/apps.py
+++ bookingapp/apps.py
@@ -4,3 +4,8 @@ from django.apps import AppConfig
 class BookingappConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'bookingapp'
+    
+    def ready(self):
+        import bookingapp.signals
+
+        
