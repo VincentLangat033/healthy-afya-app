394b55016983f55db60a5159acb35a20b1e0e06f
VincentLangat033 <kimutaiketer033@gmail.com>
2022-11-28 07:27:03 -0500

    Add a .env and .gitignore files

--- afyaapp/settings.py
+++ afyaapp/settings.py
@@ -149,8 +149,14 @@ INTERNAL_IPS = [
 
 
 # SMTP Configuration
+import environ 
+# Initialise environment variables
+env = environ.Env()
+environ.Env.read_env()
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
+EMAIL_HOST_USER = env('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = env('EMAIL_HOST_PASSWORD')
 
