41d9aac5c5a6f6aa5888dee0f801ca0a07780d24
VincentLangat033 <kimutaiketer033@gmail.com>
2022-12-09 08:23:06 +0300

    Add white noise to the middlewares

--- afyaapp/settings.py
+++ afyaapp/settings.py
@@ -72,6 +72,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     "django_browser_reload.middleware.BrowserReloadMiddleware",
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'afyaapp.urls'
